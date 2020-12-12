a411104a1be01a9c477c32f587b4d03b4f5eb723
incarnadined <51889539+incarnadined@users.noreply.github.com>
2020-12-12 17:55:35 +0000

    Added numpy approximation of e to graph

--- approximating_e.py
+++ approximating_e.py
@@ -11,10 +11,26 @@ At that point, count the number of random numbers you added together. This is th
 As you repeat trials, and average out the result of each, you converge on e.
 
 
-The graph shows the number of trials against their average result at that point (red) and the numpy recorded value for e (blue)
+The graph shows an approximation of e according to pythons built-in random module (red), an approximation according to numpy randomness (cyan), and the numpy recorded value for e (blue)
 '''
 
-def count_to_one():
+# Wrapper functions to manage arguements properly
+def random_random(low: float, high: float) -> float:
+    '''
+    Function that takes two bounds and returns a random number between them using the random module's rand function
+    '''
+    return random.uniform(low, high)
+
+def numpy_random(low: float, high: float) -> float:
+    '''
+    Function that takes returns a random number using the numpy module's rand function
+
+    The np.rand() function requires no bounds. All return values are in the range 0<=x<=1
+    '''
+    return np.random.rand()
+
+
+def count_to_one(random_function):
     '''
     A function that generates random numbers between 0 and 1 until their sum exceeds 1
 
@@ -25,7 +41,7 @@ def count_to_one():
     rand = []
 
     while t < 1:
-        rand.append(random.uniform(0.0, 1.0))
+        rand.append(random_function(0.0, 1.0))
         t += rand[-1]
 
     return len(rand)
@@ -34,27 +50,34 @@ def main():
     # Number of trials to be run. Higher it is, the more accurate e will be
     num_trials = 10000
     x = []
-    y = []
-    trials = []
+    ynp = []
+    yrandom = []
+    trials_np = []
+    trials_random = []
     
     for trial in range(1, num_trials+1):
         current_value = 0
 
-        numbers_required = count_to_one()
+        numbers_required_random = count_to_one(random_random)
+        numbers_required_np = count_to_one(numpy_random)
 
-        e_rolling_avg = (sum(trials)+numbers_required)/(len(trials)+1)
+        e_rolling_avg_np = (sum(trials_np)+numbers_required_np)/(len(trials_np)+1)
+        e_rolling_avg_random = (sum(trials_random)+numbers_required_random)/(len(trials_random)+1)
 
         x.append(trial)
-        y.append(e_rolling_avg)
-        trials.append(numbers_required)
+        ynp.append(e_rolling_avg_np)
+        yrandom.append(e_rolling_avg_random)
+        trials_random.append(numbers_required_random)
+        trials_np.append(numbers_required_np)
 
     plt.figure()
 
-    approx_e, = plt.plot(x, y, '-r', label='approximation of e')
-    e, = plt.plot(x, np.full(np.shape(y), np.e), label='numpy value of e')
+    np_e, = plt.plot(x, ynp, '-c', label='e according to numpy randomness')
+    random_e, = plt.plot(x, yrandom, '-r', label='e according to random randomness')
+    e, = plt.plot(x, np.full(np.shape(x), np.e), label='numpy value of e')
 
     # plot the legend for the different lines
-    plt.legend(handles=[approx_e, e])
+    plt.legend(handles=[np_e, random_e, e])
 
     plt.xlabel('Number of trials')
     plt.ylabel('e approximation')
